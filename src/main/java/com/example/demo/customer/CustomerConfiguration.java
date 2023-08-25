00abd6876eef7f698145d07db193bc5b134e68ed
Martin Szewieczek <e1627784@student.tuwien.ac.at>
2023-08-25 18:41:12 +0200

    add infoApp configuration class

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,13 @@ public class CustomerConfiguration {
 
     @Value("${app.useFakeRepo:false}")
     private Boolean useFakeRepo;
+
+    @Bean
+    CommandLineRunner runner(InfoApp infoApp) {
+        return args -> {
+            System.out.println(infoApp);
+        };
+    }
 
     @Bean
     CustomerRepo customerRepo() {
