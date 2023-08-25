d20294b0fbde7ec5fa61c8b180fda61c68dda0b3
Martin Szewieczek <e1627784@student.tuwien.ac.at>
2023-08-25 17:18:06 +0200

    add logging, remove banner

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,12 +1,15 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     CustomerService(CustomerRepository customerRepository) {
@@ -19,6 +22,10 @@ public class CustomerService {
 
     public Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer " + id + " not found"));
+                .orElseThrow(() -> {
+                    NotFoundException exception = new NotFoundException("customer " + id + " not found");
+                    LOGGER.error("error in getting customer {}", id, exception);
+                    return exception;
+                });
     }
 }
